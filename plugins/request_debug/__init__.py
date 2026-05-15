0730ca541d97a40736995717c851526b237f7c35
root <root@vm950545.u241867.kvm.ubuntu-22.04-amd64-r1>
2026-05-15 12:31:18 +0000

    new plugin

--- plugins/request_debug/__init__.py
+++ plugins/request_debug/__init__.py
@@ -1,11 +1,24 @@
+import logging
 import pprint
+from logging.handlers import RotatingFileHandler
 
 from flask import request
 
+LOG_FILE = "/opt/deepchan/logs/requests_debug.log"
+
 
 def init_app(app):
+    # Создаём отдельный логгер для этого плагина
+    logger = logging.getLogger("request_debug")
+    logger.setLevel(logging.INFO)
+
+    # Чтобы не дублировались записи при перезагрузке плагина
+    if not logger.handlers:
+        handler = RotatingFileHandler(LOG_FILE, maxBytes=1_000_000, backupCount=2)
+        handler.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
+        logger.addHandler(handler)
+
     def debug_request(**kwargs):
-        # Собираем всю информацию о запросе
         info = {
             "method": request.method,
             "url": request.url,
@@ -20,6 +33,6 @@ def init_app(app):
             "is_secure": request.is_secure,
             "host": request.host,
         }
-        app.logger.info(f"REQUEST DEBUG:\n{pprint.pformat(info)}")
+        logger.info(f"\n{pprint.pformat(info)}")
 
     app.on("http.before_request", debug_request)
